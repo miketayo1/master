5c2dc39bdee8d661356b7458b261f92a14328480
miketayo <miketayo@yahoo.com>
2016-05-17 16:10:09 +0100

    NotesApplications.js

---
+++ NotesApplication.js
@@ -0,0 +1,20 @@
+function NotesApplication( ){
+
+	function note(authur, lst){
+		this.note = note;
+	    this.lst= ["abc", "xyz", "lmn" ]; 
+	    return lst;
+	}
+	var create =function(note_content){
+		note_content = ["aaaaa"];
+
+	}
+	function listNotes(){
+		for (var note_id= 0 ; note_id<=lst.length; note_id++) {
+			return this.lst[note_id];
+			
+		}
+		return lst;
+
+	}
+}
